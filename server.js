91038a05569d3aa5a4faa59d43a17e6ada4a6b23
Derek Xu <derekqxu@gmail.com>
2018-07-16 18:58:17 -0700

    Add files via upload
    added naming

--- server.js
+++ server.js
@@ -7,35 +7,67 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
-// port = 8000;
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./app/routes')(app, {});
-/*
-app.listen(port, () => {
-  console.log("We are live on " + port);
-})
-*/
+
+var nsp = io.of('/my-namespace')
+var name2id = new Map();
+var id2name = new Map();
+
+nsp.on('connection', function(socket){
+  console.log('someone connected');
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+nsp.on('connection', function(socket){
+  console.log(socket.id + ' connected');
+  var name;
+  do{
+    name = 'anon' + Math.floor(Math.random() * 1001);
+  } while (name2id.get(name) != undefined)
+  name2id.set(name, socket.id);
+  id2name.set(socket.id, name);
+  nsp.emit('server message', name + ' has joined the room!');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    name2id.delete(name);
+    id2name.delete(socket.id);
+    nsp.emit('server message', name + ' has left the room!');
   });
+
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    name = id2name.get(socket.id);
+    console.log(name + ': ' + msg);
+
+    nsp.emit('chat message', name + ": " + msg);
+    if (msg.substring(0,6) == "\\name="){
+      new_name = msg.substring(6,msg.length);
+
+      if (name2id.get(socket.id) == undefined && new_name.indexOf(":") == -1){
+        nsp.emit('server message', name + ' has changed name into ' + new_name);
+        name2id.delete(socket.id)
+        name2id.set(new_name, socket.id);
+        id2name.set(socket.id, new_name);
+      }
+      else{
+        nsp.emit('server message', 'The name, ' + new_name + ', is currently being used.')
+      }
+    }
   });
+
   socket.on('update', function(msg){
-    io.emit('update', msg);
+    name = id2name.get(socket.id);
+    nsp.emit('update', name + ": " + msg);
   });
+
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:8000');
+  console.log('listening on *:3000');
 });
 
